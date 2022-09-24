63f148f500c81093c4fc5eb74479b8ec461c4b23
IlaPoly <112783000+IlaPoly@users.noreply.github.com>
2022-09-24 16:06:52 -0400

    1

--- exercice.py
+++ exercice.py
@@ -4,16 +4,44 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	first_name = name.split('-')[0]
+	lowercase = first_name.lower()
+	capitalize = lowercase.capitalize()
+	# sinon
+	# capitalize = first_name[0].upper + first_name[1:].lower
+
+	return capitalize
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	rand_animals = random.choice(animals)
+	rand_adj = random.choice(adjectives)
+	rand_fruits = random.choice(fruits)
+	sentence = ('Aujourd’hui, j’ai vu un ' + rand_animals + ' s’emparer d’un panier ' + rand_adj + ' plein de ' + rand_fruits + '.')
+	return sentence
 
 def encrypt(text, shift):
-	return ""
+	new_str = ''
+	for letter in text:
+		if ord(letter.upper()) > 64:
+			if ord(letter.upper()) > (ord('Z') - shift):
+				new_str += chr(ord(letter.upper()) - (26 - shift))
+			else:
+				new_str += chr(ord(letter.upper()) + shift)
+		else:
+			new_str += letter
+	return new_str
 
 def decrypt(encrypted_text, shift):
-	return ""
+	new_str = ''
+	for letter in encrypted_text:
+		if ord(letter) > 64:
+			if ord(letter) < (ord('A') + shift):
+				new_str += chr(ord(letter) + (26 - shift))
+			else:
+				new_str += chr(ord(letter) - shift)
+		else:
+			new_str += letter
+	return new_str
 
 
 if __name__ == "__main__":
